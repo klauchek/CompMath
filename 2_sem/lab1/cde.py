de97e5cea224ae430da0822f398382f239cf6ecd
klauchek <klauchek.md@phystech.edu>
2023-03-27 23:59:20 +0300

    finished first lab

--- 2_sem/lab1/cde.py
+++ 2_sem/lab1/cde.py
@@ -19,25 +19,41 @@ class Grid():
         self.Nt = len(self.t)
         self.Nx = len(self.x)
 
-def angl_2(grid, I):
-    u = np.zeros([grid.Nt, grid.Nx])
-    u[0] = [I(x, grid) for x in grid.x]
+def angle(grid, u):
     for i in range(grid.Nt - 1):
         for j in range(1, grid.Nx):
             u[i + 1][j] = u[i][j] * (1 - grid.Co) + grid.Co * u[i][j - 1]
         u[i + 1][0] = u[i + 1][grid.Nx - 1]
+
+
+
+
+def lax(grid):
+    u = np.zeros([grid.N, grid.M])
+    u[0] = [u0(grid, x) for x in grid.xr]
+    for i in range(grid.N-1):
+        u[i+1][0] = f(grid, u[i][grid.M-2], u[i][0], u[i][1])
+        for j in range(1, grid.M-1):
+            u[i+1][j] = f(grid, u[i][j-1], u[i][j], u[i][j+1])
+        u[i+1][grid.M-1] = f(grid, u[i][grid.M-2], u[i][grid.M-1], u[i][1])
     return u
 
 
-def LaxWendroff(Nt, Nx, Co, tau, f, x, t, u, u_n):
-
-        u_n, u = u, u_n
+def LaxWendroff(grid, u):
+    for i in range(grid.Nt - 1):
+        for j in range(1, grid.Nx - 1):
+            u[i + 1][j] = (grid.Co * (grid.Co + 1) * 0.5) * u[i][j - 1] + (1 - grid.Co**2) * u[i][j] + (grid.Co * (grid.Co - 1) * 0.5) * u[i][j + 1]
+        u[i + 1][0] = (grid.Co * (grid.Co + 1) * 0.5) * u[i][grid.Nx - 2] + (1 - grid.Co**2) * u[i][0] + (grid.Co * (grid.Co - 1) * 0.5) * u[i][1]
+        u[i + 1][grid.Nx - 1] = (grid.Co * (grid.Co + 1) * 0.5) * u[i][grid.Nx - 2] + (1 - grid.Co**2) * u[i][grid.Nx - 1] + (grid.Co * (grid.Co - 1) * 0.5) * u[i][0]
     
-def solution(grid, scheme):                
+def solution(grid, scheme):
     def I(x, grid):
         return math.sin(4 * math.pi * x / grid.L)
 
-    u = scheme(grid, I)
+    u = np.zeros([grid.Nt, grid.Nx])
+    u[0] = [I(x, grid) for x in grid.x]                
+
+    scheme(grid, u)
     return u, grid.x, grid.t
 
 
@@ -46,8 +62,6 @@ def plot_u(grid, u):
     time = 0
     i = 0
     fig, axs = plt.subplots(7)
-    print("----u-----")
-    print(u)
     while time <= T_max:
         axs[i].plot(grid.x, u[int(time / grid.tau)])
         axs[i].axis([0., grid.L, np.min(u[int(time / grid.tau)]), np.max(u[int(time / grid.tau)])])
@@ -62,16 +76,12 @@ def plot_u(grid, u):
 def plot_all():
     grids = [Grid(0.6), Grid(1), Grid(1.01)]
     for gr in grids:
-        u, x, t = solution(gr, angl_2)
+        u, x, t = solution(gr, LaxWendroff)
         plot_u(gr, u)
         plt.show()
 
-def run():
-    plot_all()
-    
-
 def main ():
-    run()
+    plot_all()
 
 if __name__ == "__main__":
     main ()
